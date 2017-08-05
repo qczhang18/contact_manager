368c32d5f608b39d26a66eccf5af677e7b3af46d
Q. Zhang <qczhang18@gmail.com>
2017-08-05 18:15:27 -0400

    validate model person with spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with first_name and last_name' do
+    person = Person.new(first_name: 'John', last_name: 'Doe')
+    expect(person).to be_valid
+  end
+
+  it 'is invalid without a first_name' do
+    person = Person.new(first_name: nil)
+    expect(person).to be_invalid
+  end
+
+  it 'is invalid without a last_name' do
+    person = Person.new(last_name: nil)
+    expect(person).to be_invalid
+  end
 end
