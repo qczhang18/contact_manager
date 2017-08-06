3868a12568aa856c265848e17320ddaf4b752de5
Q. Zhang <qczhang18@gmail.com>
2017-08-06 18:02:31 -0400

    model spec

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with presence of address attribute' do
+    email_address = FactoryGirl.create(:email_address)
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without presence of address attribute ' do
+    email_address = FactoryGirl.create(:email_address)
+    email_address.address = nil
+    expect(email_address).to be_invalid
+  end
+
 end
