aca0b5b42c5ed795f6e50dec0cb4a8c5f54a80bd
Q. Zhang <qczhang18@gmail.com>
2017-09-08 14:41:11 -0400

    refactor person model tests to include email []

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+  let(:person) do
+    Person.new(first_name: 'Alice', last_name: 'Smith')
+  end
+
   it 'is valid with first_name and last_name' do
-    person = Person.new(first_name: 'John', last_name: 'Doe')
     expect(person).to be_valid
   end
 
   it 'is invalid without a first_name' do
-    person = Person.new(first_name: nil)
+    person.first_name = nil
     expect(person).to be_invalid
   end
 
   it 'is invalid without a last_name' do
-    person = Person.new(last_name: nil)
+    person.last_name = nil
     expect(person).to be_invalid
   end
+
+  it 'is has an array of email addresses' do
+    expect(person.email_addresses).to eq([])
+  end
+
 end
